d6fdaecd0c6895898da2e58adf33e25f5dec778d
mattoofahad <mattoofahad@gmail.com>
2020-09-12 07:46:04 +0500

    Updates

---
+++ precache-manifest.9fb35a95725ca8d6531be71963ce3ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8078e5d83563c09b337f8a9f33abd5",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "984079a4564e77df2895",
+    "url": "/todo/static/css/main.c3db9137.chunk.css"
+  },
+  {
+    "revision": "a73896d069c6b450b0b9",
+    "url": "/todo/static/js/2.04fc1425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.04fc1425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984079a4564e77df2895",
+    "url": "/todo/static/js/main.11ae48ed.chunk.js"
+  },
+  {
+    "revision": "979ec1eabc1aa897c96f",
+    "url": "/todo/static/js/runtime-main.019a1efe.js"
+  }
+]);
